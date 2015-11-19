907b23a483e3340a52a0e82975372dd5af2d2c80
Miguel Castilho <miguelcastilhodias@gmail.com>
2015-11-19 00:44:27 +0000

    fixed ball bounce

---
+++ RollerBall/Assets/Physic Materials/Bouncy.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Bouncy : MonoBehaviour {
+
+	public Rigidbody rb;
+
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+
+	void OnCollisionEnter(Collision collision) {
+		rb.velocity = new Vector3(0, 8.4f, 0);
+	}
+}
