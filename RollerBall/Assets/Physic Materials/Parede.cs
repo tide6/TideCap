e52af9c8b762e28ee85a5faf2bffd0f7ac9a6737
João Esteves <jmbesteves6@gmail.com>
2015-11-25 23:57:07 +0000

    o c# parede está com erros, mas vou tratar disso ja aumentei o ground e ja pus as paredes no sitio

---
+++ RollerBall/Assets/Physic Materials/Parede.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Parede : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+
+	public float move_parede= 5;
+	// Update is called once per frame
+	void Update () {
+		Transform.Translate(Vector3.right * move_parede * Time.deltaTime);
+	}
+}
